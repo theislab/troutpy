f2d5d532f8fc8ac7690d14644bbef7165918cd56
sergiomarco25 <sergiomarco.salas@scilifelab.se>
2025-01-03 16:36:00 +0100

    mod_docstrings10

--- docs/conf.py
+++ docs/conf.py
@@ -97,6 +97,7 @@ intersphinx_mapping = {
     "scanpy": ("https://scanpy.readthedocs.io/en/stable/", None),
     "numpy": ("https://numpy.org/doc/stable/", None),
     'pandas': ('https://pandas.pydata.org/pandas-docs/stable/', None),
+    "spatialdata": ("https://spatialdata.scverse.org/en/stable/",None)
 }
 
 # List of patterns, relative to source directory, that match files and
